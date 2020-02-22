f9ec72e5300ea0111af093815295ee619e1a4195
Dan Stine <sw@stinemail.com>
2020-02-22 06:39:00 +0000

    Resource Group in separate module

--- infrastructure/pulumi/__main__.py
+++ infrastructure/pulumi/__main__.py
@@ -1,10 +1,5 @@
-import json
-
-import pulumi
-
-from pulumi_aws import resourcegroups
-
 import notf.config
+import notf.resources
 import notf.runtime
 
 # Terraform-managed project is 'sms-notifier'
@@ -14,23 +9,10 @@ tags = {
     'project': project,
 }
 
-resource_query = {
-    'query': json.dumps({
-        "ResourceTypeFilters": [
-            "AWS::AllSupported"
-        ],
-        "TagFilters": [
-            {
-                "Key": "project",
-                "Values": [project]
-            }
-        ]
-    })
-}
-
-resourcegroups.Group(project, resource_query=resource_query, tags=tags)
+notf.resources.create(project, tags)
 
 # TODO: create bucket with Pulumi
 deploy_bucket_name=notf.config.DEPLOY_BUCKET_NAME
+
 notf.runtime.create(deploy_bucket_name, tags)
 
---
+++ infrastructure/pulumi/notf/resources.py
@@ -0,0 +1,22 @@
+import json
+
+from pulumi_aws import resourcegroups
+
+def create(project, tags):
+
+    resource_query = {
+        'query': json.dumps({
+            "ResourceTypeFilters": [
+                "AWS::AllSupported"
+            ],
+            "TagFilters": [
+                {
+                    "Key": "project",
+                    "Values": [project]
+                }
+            ]
+        })
+    }
+
+    resourcegroups.Group(project, resource_query=resource_query, tags=tags)
+
