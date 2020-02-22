400f40f532a7d1631513dfb2a0a067e1adfb35f4
Dan Stine <sw@stinemail.com>
2020-02-22 06:34:55 +0000

    create Lambda itself, CloudWatch trigger

--- infrastructure/pulumi/__main__.py
+++ infrastructure/pulumi/__main__.py
@@ -4,6 +4,7 @@ import pulumi
 
 from pulumi_aws import resourcegroups
 
+import notf.config
 import notf.runtime
 
 # Terraform-managed project is 'sms-notifier'
@@ -29,5 +30,7 @@ resource_query = {
 
 resourcegroups.Group(project, resource_query=resource_query, tags=tags)
 
-notf.runtime.create_iam(tags)
+# TODO: create bucket with Pulumi
+deploy_bucket_name=notf.config.DEPLOY_BUCKET_NAME
+notf.runtime.create(deploy_bucket_name, tags)
 
--- infrastructure/pulumi/notf/runtime.py
+++ infrastructure/pulumi/notf/runtime.py
@@ -6,14 +6,36 @@ import json
 import pulumi
 import pulumi_aws as aws
 
+from pulumi_aws import cloudwatch
 from pulumi_aws import iam
+from pulumi_aws import lambda_
+
+import notf.config
 
 REGION='us-east-1'
 ACCOUNT_ID=aws.get_caller_identity().account_id
 RESOURCE_NAME='sms-notf-run-pulumi'
 
-def create_iam(tags):
 
+def create(deploy_bucket_name, tags):
+
+    # TODO: revisit this
+    log_group_name = _create_cloudwatch_log_group(tags)
+    combined = pulumi.Output.all(log_group_name, tags)
+    role = combined.apply(lambda lst: _create_iam(lst[0], lst[1]))
+
+    function = _create_lambda(deploy_bucket_name, role, tags)
+
+    _create_trigger(3, function, tags)
+    _create_trigger(4, function, tags)
+
+
+def _create_cloudwatch_log_group(tags):
+    log_group = cloudwatch.LogGroup(f'/aws/lambda/{RESOURCE_NAME}', tags=tags)
+    return log_group.name
+
+
+def _create_iam(log_group_name, tags):
     policy = json.dumps({
         "Version": "2012-10-17",
         "Statement": [
@@ -24,7 +46,7 @@ def create_iam(tags):
                     "logs:PutLogEvents"
                 ],
                 "Resource": [
-                    f"arn:aws:logs:{REGION}:{ACCOUNT_ID}:log-group:${RESOURCE_NAME}:*"
+                    f"arn:aws:logs:{REGION}:{ACCOUNT_ID}:log-group:{log_group_name}:*"
                 ]
             },
             {
@@ -56,4 +78,60 @@ def create_iam(tags):
     role = iam.Role(RESOURCE_NAME, assume_role_policy=assume_role_policy, tags=tags)
 
     role_policy_attachment = iam.RolePolicyAttachment(RESOURCE_NAME, policy_arn=iam_policy.arn, role=role)
+
+    return role
+
+
+def _create_lambda(deploy_bucket_name, role, tags):
+
+    function = lambda_.Function(
+        RESOURCE_NAME,
+        role=role.arn,
+        handler="com.github.dstine.sms.SmsHandler::handleRequest",
+        runtime="java8",
+        memory_size="256",
+        timeout="60",
+        s3_bucket=deploy_bucket_name,
+        s3_key="sms-notifier.zip",
+        environment = {
+            'variables': {
+                'SMTP_HOST':     notf.config.SMTP_HOST,
+                'SMTP_USERNAME': notf.config.SMTP_USERNAME,
+                'SMTP_PASSWORD': notf.config.SMTP_PASSWORD,
+                'EMAIL_SUBJECT': notf.config.EMAIL_SUBJECT,
+                'EMAIL_FROM':    notf.config.EMAIL_FROM,
+                'EMAIL_TO':      notf.config.EMAIL_TO,
+            }
+        },
+        tags=tags,
+    )
+    return function
+
+
+def _create_trigger(id, function, tags):
+
+    trigger = notf.config.TRIGGERS[id]
+    event_name = f'{RESOURCE_NAME}-{id}'
+
+    event_rule = cloudwatch.EventRule(
+        event_name,
+        schedule_expression=trigger['schedule']
+    )
+
+    event_target = cloudwatch.EventTarget(
+        event_name,
+        rule=event_rule,
+        target_id=function.name,
+        arn=function.arn,
+        input=trigger['input'],
+    )
+
+    lambda_.Permission(
+        event_name, # RESOURCE_NAME?
+        statement_id=event_name,
+        action="lambda:InvokeFunction",
+        function=function,
+        principal="events.amazonaws.com",
+        source_arn=event_rule.arn,
+    )
 
