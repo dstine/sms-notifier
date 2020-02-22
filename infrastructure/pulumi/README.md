76b1d3f13c2124750d37731a4a6e68309550121e
Dan Stine <sw@stinemail.com>
2020-02-22 03:56:12 +0000

    initial Pulumi scaffolding

---
+++ infrastructure/pulumi/README.md
@@ -0,0 +1,16 @@
+
+
+```
+$ ./run-pulumi.sh login --local
+```
+
+```
+$ ./run-pulumi.sh plugin install resource aws v1.23.0
+```
+
+```
+$ python3 -m venv venv
+$ source venv/bin/activate
+$ pip install -r requirements.txt
+```
+
---
+++ infrastructure/pulumi/run-pulumi.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# https://www.pulumi.com/docs/reference/cli/environment-variables/
+export PULUMI_CONFIG_PASSPHRASE=''
+export PULUMI_HOME='.pulumi'
+export PULUMI_SKIP_UPDATE_CHECK=true
+
+PULUMI_VERSION=1.11.0
+PULUMI_BIN=~/software/pulumi/pulumi-v${PULUMI_VERSION}
+
+AWS_PROFILE=terraform
+export AWS_PROFILE=${AWS_PROFILE}
+echo "AWS_PROFILE:" ${AWS_PROFILE}
+
+PULUMI=${PULUMI_BIN}/pulumi
+
+if [ -z $1 ]; then
+  DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+  echo "Error in $DIR/`basename $0`:"
+  echo "  Must set 1st argument to PULUMI_CMD"
+  exit 1
+else
+  PULUMI_CMD=$1
+fi
+
+PULUMI_ARGS=${@:2}
+
+CLI="${PULUMI} ${PULUMI_CMD} ${PULUMI_ARGS}"
+echo ${CLI}
+${CLI}
+
