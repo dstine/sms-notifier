09e51c531dba840f79aa21c4cb230dba0d6e531e
Dan Stine <sw@stinemail.com>
2020-02-23 20:17:31 +0000

    remove deploy time user

--- infrastructure/pulumi/notf/deploytime.py
+++ infrastructure/pulumi/notf/deploytime.py
@@ -16,7 +16,6 @@ def create(tags):
 
     deploy_bucket_name = _create_s3(tags)
     # TODO: upload Java code via dynamic provider
-    deploy_bucket_name.apply(lambda dbn: _create_iam(dbn, tags))
     return deploy_bucket_name
 
 
@@ -41,61 +40,5 @@ def _create_s3(tags):
     pulumi.export('bucket_name', bucket.bucket)
     deploy_bucket_name = bucket.bucket
     return deploy_bucket_name
-
-
-def _create_iam(deploy_bucket_name, tags):
-
-    user = iam.User(
-        RESOURCE_NAME,
-        path='/',
-        tags=tags,
-    )
-
-    group = iam.Group(
-        RESOURCE_NAME,
-        path='/',
-    )
-
-    iam.GroupMembership(
-        RESOURCE_NAME,
-        users=[user],
-        group=group,
-    )
-
-    policy = json.dumps({
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Effect": "Allow",
-                "Action": [
-                    "s3:ListAllMyBuckets"
-                ],
-                "Resource": "arn:aws:s3:::*"
-            },
-            {
-                "Effect": "Allow",
-                "Action": [
-                    "s3:*"
-                ],
-                "Resource": [
-                    f"arn:aws:s3:::{deploy_bucket_name}",
-                    f"arn:aws:s3:::{deploy_bucket_name}/*"
-                ]
-            }
-        ]
-    })
-
-    iam_policy = iam.Policy(
-        RESOURCE_NAME,
-        path='/',
-        policy=policy,
-    )
-
-    iam.GroupPolicyAttachment(
-        RESOURCE_NAME,
-        policy_arn=iam_policy.arn,
-        group=group,
-    )
-
 
 
