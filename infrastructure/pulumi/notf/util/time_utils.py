9c8f97c90012e58e1ddf3c18a067c238072bbba7
Dan Stine <sw@stinemail.com>
2020-02-28 22:37:29 +0000

    improve args of UTC function

--- infrastructure/pulumi/notf/util/time_utils.py
+++ infrastructure/pulumi/notf/util/time_utils.py
@@ -4,9 +4,10 @@ import pytz
 
 LOCAL_TZ = pytz.timezone('America/New_York')
 
-def to_utc(h_local, m_local):
+def to_utc(hh_mm_local):
+    h_local, m_local = hh_mm_local.split(':')
     now = datetime.datetime.now(tz=LOCAL_TZ)
-    local = now.replace(hour=h_local, minute=m_local)
+    local = now.replace(hour=int(h_local), minute=int(m_local))
     utc = local.astimezone(pytz.utc)
     return f'{utc.minute} {utc.hour}'
 
