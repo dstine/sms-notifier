211f77116a70aa3e5d48bd56e9117affccb6444d
Dan Stine <sw@stinemail.com>
2020-03-09 00:58:37 +0000

    fix tests for DST

--- infrastructure/pulumi/tests/notf/util/time_utils_test.py
+++ infrastructure/pulumi/tests/notf/util/time_utils_test.py
@@ -1,8 +1,9 @@
 import notf.util.time_utils
 
 def test_to_utc():
-    assert '0 13' == notf.util.time_utils.to_utc('8:00')
-    assert '0 19' == notf.util.time_utils.to_utc('14:00')
-    assert '15 21' == notf.util.time_utils.to_utc('16:15')
-    assert '15 4' == notf.util.time_utils.to_utc('23:15')
+    # TODO: account for DST
+    assert '0 12' == notf.util.time_utils.to_utc('8:00')
+    assert '0 18' == notf.util.time_utils.to_utc('14:00')
+    assert '15 20' == notf.util.time_utils.to_utc('16:15')
+    assert '15 3' == notf.util.time_utils.to_utc('23:15')
 
